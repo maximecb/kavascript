1cb1aa5969977aeb83c2413c9050273618c26952
Maxime Chevalier-Boisvert <maximechevalierb@gmail.com>
2021-05-18 00:25:45 -0400

    Progress on parsing code

--- main.rs
+++ main.rs
@@ -1,5 +1,6 @@
 #![allow(unused_imports)]
 #![allow(dead_code)]
+#![allow(unused_variables)]
 
 mod vm;
 use vm::*;
@@ -9,7 +10,18 @@ use parser::*;
 
 fn main()
 {
+    println!("Hello");
+
     let _x = vm::Instr::Push{ val:3 };
+
+
+    let _input = parser::Input::new("test src str".to_string(), "input".to_string());
+
+
+    let _b = '0' == '1';
+    let _b2 = '0' < '1';
+    let _b2 = '0' < 0x95 as char;
+    let _x = '0' as u8;
 
 
 
